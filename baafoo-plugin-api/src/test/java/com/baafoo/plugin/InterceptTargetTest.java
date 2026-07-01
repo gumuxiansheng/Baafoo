ce50ebf5973db15b7479cc5b1dfaa3bca454887b
mike <zhufuyuan@mikezhu.cn>
2026-07-01 22:31:48 +0800

    test(plugin-api): update InterceptTargetTest for GRPC enum value
    The GRPC value was added to InterceptTarget (P1-4 gRPC support) but the count assertion was not updated, causing the test to fail with expected:<8> but was:<9> when building with -am. Adds the GRPC valueOf assertion alongside the count fix.

--- baafoo-plugin-api/src/test/java/com/baafoo/plugin/InterceptTargetTest.java
+++ baafoo-plugin-api/src/test/java/com/baafoo/plugin/InterceptTargetTest.java
@@ -7,12 +7,13 @@ public class InterceptTargetTest {
 
     @Test
     public void testValues() {
-        assertEquals(8, InterceptTarget.values().length);
+        assertEquals(9, InterceptTarget.values().length);
         assertEquals(InterceptTarget.SOCKET, InterceptTarget.valueOf("SOCKET"));
         assertEquals(InterceptTarget.NIO_SOCKET, InterceptTarget.valueOf("NIO_SOCKET"));
         assertEquals(InterceptTarget.KAFKA, InterceptTarget.valueOf("KAFKA"));
         assertEquals(InterceptTarget.PULSAR, InterceptTarget.valueOf("PULSAR"));
         assertEquals(InterceptTarget.JMS, InterceptTarget.valueOf("JMS"));
+        assertEquals(InterceptTarget.GRPC, InterceptTarget.valueOf("GRPC"));
         assertEquals(InterceptTarget.CONSUL_DNS, InterceptTarget.valueOf("CONSUL_DNS"));
         assertEquals(InterceptTarget.CONSUL_API, InterceptTarget.valueOf("CONSUL_API"));
         assertEquals(InterceptTarget.FEIGN, InterceptTarget.valueOf("FEIGN"));
